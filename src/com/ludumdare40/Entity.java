a1361a21aed645ce43437163186b48512ca3057a
Langston Nashold <langston201@gmail.com>
2017-12-02 10:15:20 -0600

    enitity

--- src/com/ludumdare40/Entity.java
+++ src/com/ludumdare40/Entity.java
@@ -5,15 +5,30 @@ import org.newdawn.slick.Image;
 
 public class Entity {
 	private float x;
+
+	public float getY() {
+		return y;
+	}
+
+	public Image getImg() {
+		return img;
+	}
+
 	private float y;
 	private Image img;
 	
 	public	Entity(float x, float y, Image img) {
-		
+		this.x = x;
+		this.y = y;
+		this.img = img;
 	}
-	
+
+	public float getX() {
+		return x;
+	}
+
 	public void render(Graphics g) {
-		
+
 	}
 	
 	public void update(int delta) {
