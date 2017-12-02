217f08ee1cb3fceca24d2a24b2d54fbc7ef5a16c
Langston Nashold <langston201@gmail.com>
2017-12-02 10:41:14 -0600

    asdf;l

---
+++ src/com/ludumdare40/Monster.java
@@ -0,0 +1,9 @@
+package com.ludumdare40;
+
+import org.newdawn.slick.Image;
+
+public class Monster extends Entity {
+    public Monster(int x, int y, Image img) {
+        super(x, y, img);
+    }
+}
--- src/com/ludumdare40/staticEntity.java
+++ src/com/ludumdare40/staticEntity.java
@@ -2,26 +2,33 @@ package com.ludumdare40;
 
 import org.newdawn.slick.Image;
 
-public class staticEntity extends Entity{
+public class StaticEntity extends Entity{
     public enum Type{TREE,ROCK}
     private final String treeImgLocation = "res/Tree.png";
     private final String rockImgLocation = "res/Rock.png";
 
-    public staticEntity(float x, float y, Type type){
-        super(x, y);
-        switch(type){
+    public StaticEntity(float x, float y, Type type, Image img) {
+        super(x, y, img);
+        switch (type) {
             case TREE:
-                img = loadImage(treeImgLocation);
+                try {
+                    img = new Image(treeImgLocation);
+                } catch (Exception e) {
+                    System.out.println("Could not load the tree Image");
+                }
                 collider.setWidth(32);
                 collider.setHeight(32);
                 break;
             case ROCK:
-                img = loadImage(rockImgLocation);
+                try {
+                    img = new Image(rockImgLocation);
+                } catch (Exception e) {
+                    System.out.println("Could not load the Rock Image");
+                }
                 collider.setWidth(32);
                 collider.setHeight(32);
                 break;
         }
     }
-
     
 }
